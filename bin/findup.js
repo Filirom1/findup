d7f77c7df27ec0c57b228a5d2b15594e67e82e2d
Romain Philibert <filirom1@gmail.com>
2012-04-21 10:46:18 +0200

    refactor with EventEmitter

--- bin/findup.js
+++ bin/findup.js
@@ -1,15 +1,52 @@
 #!/usr/bin/env node
 
 var findup = require('..'),
-  Path = require('path');
+  Path = require('path'),
+  nopt = require('nopt'),
+  knownOpts = {
+    name      : String,
+    dir       : Path,
+    help      : Boolean,
+    verbose   : Boolean
+  },
+  description = {
+    name      : "The name of the file to found",
+    dir  : "The directoy where we will start walking up",
+    help      : "show usage",
+    verbose   : "print log"
 
-if(process.argv.length < 3) {
+  },
+  defaults = {
+    dir       : process.cwd(),
+    help      : false,
+    verbose   : false
+  },
+  shortHands = {
+    d  : "--dir",
+    n  : "--name",
+    h  : "--help",
+    v  : "--verbose"
+  },
+  options = nopt(knownOpts, shortHands, process.argv, 2),
+  argvRemain = options.argv.remain;
+
+// defaults value
+Object.keys(defaults).forEach(function(key){
+  var value = defaults[key];
+  options[key] = options[key] || value;
+});
+
+if(argvRemain && argvRemain.length >=1 ) options.name = argvRemain[0];
+
+if(!options.name) {
   console.error('Usage: findup [FILE]');
+  console.error('');
+  console.error(nopt.usage(knownOpts, shortHands, description, defaults));
   process.exit(-1);
 }
 
-var file = process.argv[2];
-findup(process.cwd(), file, function(err, dir){
+var file = options.name;
+findup(process.cwd(), file, options, function(err, dir){
   if(err) return console.error(err.message ? err.message : err);
-  console.log(Path.join(dir, file))
+  console.log(Path.join(dir, file));
 });
--- index.js
+++ index.js
@@ -1,29 +1,85 @@
 var fs = require('fs'),
-  Path = require('path');
+  Path = require('path'),
+  util = require('util'),
+  colors = require('colors'),
+  EE = require('events').EventEmitter;
 
-module.exports = function findup(dir, iterator, cb){
+module.exports = function(dir, iterator, options, callback){
+  return new FindUp(dir, iterator, options, callback);
+};
+
+function FindUp(dir, iterator, options, callback){
+  if (!(this instanceof FindUp)) {
+    return new FindUp(dir, iterator, options, callback);
+  }
+  if(typeof options === 'function'){
+    callback = options;
+    options = {};
+  }
+
+  EE.call(this);
+  this.found = false;
+  this.stopPlease = false;
+  var self = this;
+
   if(typeof iterator === 'string'){
     var file = iterator;
     iterator = function(dir, cb){
       return Path.exists(Path.join(dir, file), cb);
-    }
+    };
   }
-  cb = cb || function(){};
 
-  if (dir === '/') return cb(new Error('not found'));
-  if(dir.indexOf('../../') !== -1 ) return cb(new Error(dir + ' is not correct.'));
-  iterator(dir, function(exists){
-    if(exists) return cb(null, dir);
-    findup(Path.join(dir, '..'), iterator, cb);
-  });
+  if(callback) {
+    this.on('found', function(dir){
+      if(options.verbose) console.log(('found '+ dir ).green);
+      callback(null, dir);
+      self.stop();
+    });
+
+    this.on('end', function(){
+      if(options.verbose) console.log('end'.grey);
+      if(!self.found) callback(new Error('not found'));
+    });
+
+    this.on('error', function(err){
+      if(options.verbose) console.log('error'.red, err);
+      callback(err);
+    });
+  }
+
+  this._find(dir, iterator, options, callback);
 }
+util.inherits(FindUp, EE);
+
+FindUp.prototype._find = function(dir, iterator, options, callback){
+  var self = this;
+
+  iterator(dir, function(exists){
+    if(options.verbose) console.log(('traverse '+ dir).grey);
+    if(exists) {
+      self.found = true;
+      self.emit('found', dir);
+    }
+
+    if (self.stopPlease) return self.emit('end');
+    if (dir === '/') return self.emit('end');
+    if(dir.indexOf('../../') !== -1 ) return self.emit('error', new Error(dir + ' is not correct.'));
+    self._find(Path.join(dir, '..'), iterator, options, callback);
+  });
+};
+
+FindUp.prototype.stop = function(){
+  this.stopPlease = true;
+};
+
+module.exports.FindUp = FindUp;
 
 module.exports.sync = function(dir, iteratorSync){
   if(typeof iteratorSync === 'string'){
     var file = iteratorSync;
     iteratorSync = function(dir){
       return Path.existsSync(Path.join(dir, file));
-    }
+    };
   }
   var initialDir = dir;
   while(dir !== '/'){
